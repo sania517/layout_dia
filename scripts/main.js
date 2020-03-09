7f5fe263a13c55eee4daac8340e1281b0634d1b9
Olexander <kravchenko.sania@gmail.com>
2020-03-09 15:21:46 +0200

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -11,3 +11,40 @@ function initMenu() {
 }
 
 initMenu();
+
+function sliderStart() {
+  const slider = document.querySelector('.slider');
+  const cards = Array.from(document.querySelectorAll('.slider__item'));
+
+  let currentIndex = cards.findIndex(item => {
+    return item.classList.contains('slider__item--active');
+  });
+
+  function handler(e) {
+    if (!e.target.classList.contains('slider__button')) {
+      return;
+    }
+
+    cards[currentIndex].classList.remove('slider__item--active');
+
+    if (e.target.classList.contains('slider__button-prev')) {
+      if (currentIndex === 0) {
+        currentIndex = cards.length - 1;
+      } else {
+        currentIndex -= 1;
+      }
+    } else if (e.target.classList.contains('slider__button-next')) {
+      if (currentIndex === cards.length - 1) {
+        currentIndex = 0;
+      } else {
+        currentIndex += 1;
+      }
+    }
+
+    cards[currentIndex].classList.add('slider__item--active');
+  }
+
+  slider.addEventListener('click', handler);
+}
+
+sliderStart();
